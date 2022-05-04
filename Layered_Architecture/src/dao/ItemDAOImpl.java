d173799e478a9d1bf9fd185ae65ccdc63264fde7
VihangaIndeewra <indeewravihnga@gmail.com>
2022-05-04 15:49:39 +0530

    Refactor Save Item

--- Layered_Architecture/src/dao/ItemDAOImpl.java
+++ Layered_Architecture/src/dao/ItemDAOImpl.java
@@ -36,6 +36,15 @@ public class ItemDAOImpl {
         }
 
 
+        public boolean saveItem(ItemDTO item) throws SQLException, ClassNotFoundException {
+            Connection connection = DBConnection.getDbConnection().getConnection();
+            PreparedStatement pstm = connection.prepareStatement("INSERT INTO Item (code, description, unitPrice, qtyOnHand) VALUES (?,?,?,?)");
+            pstm.setString(1, item.getCode());
+            pstm.setString(2, item.getDescription());
+            pstm.setBigDecimal(3, item.getUnitPrice());
+            pstm.setInt(4, item.getQtyOnHand());
+            return pstm.executeUpdate()>0;
+        }
 
     }
 
